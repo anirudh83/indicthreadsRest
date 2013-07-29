3bb31367a5b3bd56db0f69c53bd39345c73560c2
Anirudh <anirudh.bh@gmail.com>
2013-07-29 18:34:03 +0530

    initial commit

---
+++ src/main/java/com/anirudh/ws/service/CustomerResource.java
@@ -0,0 +1,87 @@
+package com.anirudh.ws.service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+
+import com.anirudh.ws.domain.Customer;
+import com.anirudh.ws.util.Util;
+
+
+/**
+ * 
+ * @author anirudh
+ *
+ */
+@Path("/customers")
+public class CustomerResource {
+	
+	private Map<Integer,Customer> customerDB = new ConcurrentHashMap<Integer,Customer>();
+	
+	private AtomicInteger idCounter = new AtomicInteger();
+	
+	@POST
+	@Consumes("application/xml")
+	public Response createCustomer(InputStream is){
+		Customer customer = new Customer();
+		customer.setId(idCounter.incrementAndGet());
+		customerDB.put(customer.getId(),customer);
+		return Response.created(URI.create("/customers/"
+				+customer.getId())).build();
+		
+	}
+	
+	@GET
+	@Path("{id}")
+	@Produces("application/xml") // This annotation tells JAX-RS which content-type GET reponse will be.
+	//PathParam will take care to convert String id coming from URL into int.
+	public StreamingOutput getCustomer(@PathParam("id") int id){
+		final Customer customer = customerDB.get(id);
+		if(customer == null){
+			throw new WebApplicationException(Response.Status.NOT_FOUND);
+		}
+		return new StreamingOutput(){
+			public void write(OutputStream outputStream) throws IOException,
+					WebApplicationException {
+				Util.outputCustomer(outputStream, customer);
+			}
+		};
+	}
+	
+	@PUT
+	@Path("{id}")
+	@Consumes("application/xml")
+	public void updateCustomer(@PathParam("id") int id, InputStream is){
+		
+		Customer updated = Util.readCustomer(is);
+		Customer current = customerDB.get(id);
+		if(current == null){
+			throw new WebApplicationException(Response.Status.NOT_FOUND);
+		}
+		
+		//copy attributes from updated to current
+		current.setFirstName(updated.getFirstName());
+		current.setLastName(updated.getLastName());
+		current.setStreet(updated.getStreet());
+		current.setCity(updated.getCity());
+		current.setZip(updated.getZip());
+		current.setCountry(updated.getCountry());
+		
+	}
+
+}
---
+++ src/main/java/com/anirudh/ws/util/Util.java
@@ -0,0 +1,64 @@
+package com.anirudh.ws.util;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import com.anirudh.ws.domain.Customer;
+
+/**
+ * 
+ * @author anirudh
+ * this class is used as we are not using JAXB to convert XML into object and object into XML(marshelling and unmarshelling)
+ */
+public class Util {
+
+	public static void outputCustomer(OutputStream os, Customer cust) {
+
+		PrintStream writer = new PrintStream(os);
+		writer.println("<customer id=\"" + cust.getId() + "\">");
+		writer.println(" <first-name>" + cust.getFirstName() + "</first-name>");
+		writer.println(" <last-name>" + cust.getLastName() + "</last-name>");
+		writer.println("</customer>");
+
+	}
+
+	public static Customer readCustomer(InputStream is) {
+		try {
+			DocumentBuilder builder = DocumentBuilderFactory.newInstance()
+					.newDocumentBuilder();
+			Document doc = builder.parse(is);
+			Element root = doc.getDocumentElement();
+
+			Customer cust = new Customer();
+			if (root.getAttribute("id") != null
+					&& !root.getAttribute("id").trim().equals("")) {
+				cust.setId(Integer.valueOf(root.getAttribute("id")));
+			}
+
+			NodeList nodes = root.getChildNodes();
+			for (int i = 0; i < nodes.getLength(); i++) {
+				Element element = (Element) nodes.item(i);
+
+				if (element.getTagName().equals("first-name")) {
+					cust.setFirstName(element.getTextContent());
+				} else if (element.getTagName().equals("last-name")) {
+					cust.setLastName(element.getTextContent());
+				}
+			}
+			return cust;
+		} catch (Exception e) {
+			throw new WebApplicationException(e, Response.Status.BAD_REQUEST);
+		}
+	}
+
+}
